f473f5b741640cf321af219fafb6b6f24242b47b
Murillo C Leta <murillocasemiro@gmail.com>
2022-01-21 14:31:58 -0300

    Projeto prático

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+            
+            DateTime dataFimPagamento = new DateTime(2022,02,20);
+
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(60);
+                //dataFimPagamento - dataCorrente;
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+
+            string msg = "Vencimento em : " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(msg);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ByteBank004/ByteBank_SistemaInterno/Program.cs
@@ -0,0 +1,23 @@
+﻿using ByteBank.Modelos;
+using System;
+
+namespace ByteBank_SistemaInterno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ContaCorrente conta = new ContaCorrente(123, 456789);
+
+            Console.WriteLine(conta.Saldo);
+
+            conta.Sacar(10);
+
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
